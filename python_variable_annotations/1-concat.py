72426965b440ee0ca1384d3685a0c12dc572991a
juansechaux <juansechaux@hotmail.com>
2024-01-10 09:45:07 -0500

    Task 1, type-annotated function concat that takes a string str1 and a string str2 as arguments and returns a concatenated string

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+type-annotated function add that takes a
+float a and a float b as arguments and
+returns their sum as a float
+"""
+
+
+def add(a: float, b: float) -> float:
+    '''add function that takes two floats and return
+    a float'''
+    return (a + b)
