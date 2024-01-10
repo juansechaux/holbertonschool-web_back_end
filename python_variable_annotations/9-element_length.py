a44b7185feffb7d9b69a6817d4a0b63384193138
juansechaux <juansechaux@hotmail.com>
2024-01-10 12:26:01 -0500

    Task 9, Annotate the functions parameters and return values with the appropriate types

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python3
 """
-type-annotated function make_multiplier that takes a float
-multiplier as argument and returns a function that
-multiplies a float by multiplier
+functions parameters and return values with the appropriate types
 """
 from typing import List, Union, Tuple, Callable, Iterable, Sequence
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """
+    Calculates the length of each element in the input iterable.
+
+    Parameters:
+    - lst (Iterable[Sequence]): An iterable containing elements of type Sequence.
+
+    Returns:
+    - List[Tuple[Sequence, int]]: A list of tuples where each tuple contains an element from the input iterable
+      and its corresponding length.
+    """
     return [(i, len(i)) for i in lst]
