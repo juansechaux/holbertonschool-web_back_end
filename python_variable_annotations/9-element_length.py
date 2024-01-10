7a65d2e439735ac093e1a4fd28be6897a191f261
juansechaux <juansechaux@hotmail.com>
2024-01-10 12:27:12 -0500

    Task 9, Annotate the functions parameters and return values with the appropriate types

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -10,10 +10,12 @@ def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     Calculates the length of each element in the input iterable.
 
     Parameters:
-    - lst (Iterable[Sequence]): An iterable containing elements of type Sequence.
+    - lst (Iterable[Sequence]): An iterable containing
+    elements of type Sequence.
 
     Returns:
-    - List[Tuple[Sequence, int]]: A list of tuples where each tuple contains an element from the input iterable
+    - List[Tuple[Sequence, int]]: A list of tuples where
+    each tuple contains an element from the input iterable
       and its corresponding length.
     """
     return [(i, len(i)) for i in lst]
