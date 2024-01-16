389b65af377c45e100d6472eb95cb48ee5b056c8
juansechaux <juansechaux@hotmail.com>
2024-01-15 19:56:59 -0500

    Task 9, Python function that inserts a new document in a collection based on kwargs

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -8,6 +8,5 @@ def insert_school(mongo_collection, **kwargs):
     '''
     Python function that inserts a new document in a collection based on kwargs
     '''
-    for key, value in kwargs.items():
-        result = mongo_collection.insert_one({key: value})
-    return result.inserted_id
+    results = mongo_collection.insert_many([kwargs])
+    return results.inserted_ids
