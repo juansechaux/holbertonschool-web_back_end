ba2df4d8b91c89713ff5f87a044d83837de4c72a
juansechaux <juansechaux@hotmail.com>
2024-01-15 20:20:37 -0500

    Task 11, Python function that returns the list of school having a specific topic

---
+++ NoSQL/11-schools_by_topic.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''
+Python function that returns the list of school having a specific topic
+'''
+
+
+def schools_by_topic(mongo_collection, topic):
+    '''
+    Python function that returns the list of school having a specific topic
+    '''
+    result = mongo_collection.find({"topics": topic})
+    return result
