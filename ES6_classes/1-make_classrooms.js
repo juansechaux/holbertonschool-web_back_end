89b42d245fbb361cb942369625936b667974d02e
juansechaux <juansechaux@hotmail.com>
2024-01-22 18:41:12 -0500

    Task 1, function named initializeRooms. It should return an array of 3 ClassRoom objects with the sizes 19, 20, and 34

---
+++ ES6_classes/1-make_classrooms.js
@@ -0,0 +1,10 @@
+import ClassRoom from './0-classroom';
+
+export default function initializeRooms() {
+  const arrayOfClassrooms = [];
+  for (let x = 19; x < 21; x++) {
+    arrayOfClassrooms.push(new ClassRoom(x));
+  }
+  arrayOfClassrooms.push(new ClassRoom(34));
+  return arrayOfClassrooms;
+}
