f38fc7765336563fb140869811eaa46b93956742
juansechaux <juansechaux@hotmail.com>
2024-01-23 17:17:21 -0500

    Task 7, class named Airport. Constructor attributes, name (String), code (String), use the simbol type Symbol.toStringTag

---
+++ ES6_classes/7-airport.js
@@ -0,0 +1,38 @@
+export default class Airport {
+  constructor(name, code) {
+    this._name = '';
+    this._code = '';
+
+    this.name = name;
+    this.code = code;
+  }
+
+  get [Symbol.toStringTag]() {
+    return `${this._code}`;
+  }
+  //  toString() {
+  //    return `[object ${this._code}]`;
+  // }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = value;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._code = value;
+  }
+}
