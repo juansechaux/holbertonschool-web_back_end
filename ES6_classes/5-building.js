ce895ece87e51e1743380555cba2067c912348af
juansechaux <juansechaux@hotmail.com>
2024-01-23 16:48:11 -0500

    Task 5, ajust the method evacuationWarningMessage. Task 6, class named SkyHighBuilding that extends from Building. Constructor attributes, sqft (Number) (assigned to the parent class Building), floors (Number). Override the method named evacuationWarningMessage and return the following string Evacuate slowly the NUMBER_OF_FLOORS floors.

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,16 +1,11 @@
 export default class Building {
   constructor(sqft) {
-    if (this.constructor !== Building) {
+    if (this.constructor !== Building && this.evacuationWarningMessage === undefined) {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
     this._sqft = 0;
 
     this.sqft = sqft;
-  }
-
-  /* eslint-disable class-methods-use-this */
-  evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
   }
 
   get sqft() {
---
+++ ES6_classes/6-sky_high.js
@@ -0,0 +1,25 @@
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = 0;
+
+    this.floors = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  set floors(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Floors must be a number');
+    }
+    this._floors = value;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
+}
