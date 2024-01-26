a663ebe118c65dfb4129924cc9807e8335a8aa53
juansechaux <juansechaux@hotmail.com>
2024-01-26 16:33:28 -0500

    Task 2

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,10 +1,16 @@
 export default function handleResponseFromAPI(promise) {
-  return new Promise((resolve, reject) => {
-    if (promise) {
-      resolve({ status: 200, body: 'Success' });
-    } else {
-      reject(new Error());
-    }
-    console.log('Got a response from the API');
-  });
-}
+    return promise
+      .then((result) => {
+        console.log('Got a response from the API');
+        // Return an object for resolved promise
+        return {
+          status: 200,
+          body: 'success'
+        };
+      })
+      .catch((error) => {
+        console.error('Error:', error.message);
+        // Return an empty Error object for rejected promise
+        return new Error();
+      });
+  }
