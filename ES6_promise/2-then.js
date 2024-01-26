f5b29eabbdba8ca31cb927892d3fbaf4cd0efc2d
juansechaux <juansechaux@hotmail.com>
2024-01-26 16:37:26 -0500

    Task 2, method .then and .catch

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,16 +1,16 @@
 export default function handleResponseFromAPI(promise) {
-    return promise
-      .then((result) => {
-        console.log('Got a response from the API');
-        // Return an object for resolved promise
-        return {
-          status: 200,
-          body: 'success'
-        };
-      })
-      .catch((error) => {
-        console.error('Error:', error.message);
-        // Return an empty Error object for rejected promise
-        return new Error();
-      });
-  }
+  return promise
+    .then(() => {
+      console.log('Got a response from the API');
+      // Return an object for resolved promise
+      return {
+        status: 200,
+        body: 'success',
+      };
+    })
+    .catch((error) => {
+      console.error('Error:', error.message);
+      // Return an empty Error object for rejected promise
+      return new Error();
+    });
+}
