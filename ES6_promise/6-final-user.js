6e3db7f0533ebc8ec1637b8f419a445b13f4939c
juansechaux <juansechaux@hotmail.com>
2024-01-26 19:06:32 -0500

    Task 6, use the method Promise.allSettled

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -3,7 +3,7 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((values) => (values))
+    .then((values) => values)
     .catch(() => {
       console.log('Signup system offline');
     });
